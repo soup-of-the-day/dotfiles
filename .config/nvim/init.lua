9d52851e2df9c3174f43261f264d0276a0df4538
Nicholas Smith <nicholas.smith.dev@gmail.com>
2025-03-15 09:32:37 -0400

    Load repo specific lua code
    - function looks for a .nvim.lua file in the repo and will execute that
    lua code

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -132,18 +132,8 @@ vim.opt.rtp:prepend(lazypath)
 --
 --  To update plugins you can run
 --    :Lazy update
---
--- NOTE: Here is where you install your plugins.
 require('lazy').setup({
-  -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
-
-  -- NOTE: Plugins can also be added by using a table,
-  -- with the first argument being the link and the following
-  -- keys can be used to configure plugin behavior/loading/etc.
-  --
-  -- Use `opts = {}` to force a plugin to be loaded.
-  --
 
   -- Here is a more advanced example where we pass configuration
   -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
@@ -808,5 +798,7 @@ require('lazy').setup({
   },
 })
 
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
+local local_config = vim.fn.getcwd() .. '/.nvim.lua'
+if vim.fn.filereadable(local_config) == 1 then
+  dofile(local_config)
+end
